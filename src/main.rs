6c568f80825e17fa5e8a31fa5c78227d6b05c0b0
eduardo escobar <war_army_10@hotmail.com>
2024-08-07 00:00:45 -0600

    1.4 Nice Error

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::read_to_string;
+use std::path::PathBuf;
+use anyhow::{Context, Result};
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: PathBuf
+}
+fn main() -> Result<()> {
+    let args = Cli::parse();
+
+    let result = read_to_string(&args.path)
+        .with_context(|| format!("could not read file '{}'", args.path.display()))?;
+
+    for line in result.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
+    Ok(())
+
 }
