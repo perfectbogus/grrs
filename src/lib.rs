f3cba404d0290f42d52ab1f879ce51cf2cff35e7
eduardo escobar <war_army_10@hotmail.com>
2024-08-08 23:40:02 -0600

    finish this project

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,12 @@ fn answer() -> i8 {
 
 #[cfg(test)]
 mod test {
+    use std::error::Error;
+    use assert_fs::prelude::*;
     use crate::{answer, find_matches};
+    use assert_cmd::prelude::*; // Add methods on commands
+    use predicates::prelude::*; // Used for writing assertions
+    use std::process::Command; // Run programs
 
     #[test]
     fn check_answer_validity() {
@@ -29,5 +34,31 @@ mod test {
         find_matches("this is a text\n in a test", "text", &mut result);
         assert_eq!(result, b"this is a text");
     }
+
+    #[test]
+    fn content_in_file() -> Result<(), Box<dyn Error>> {
+        let file = assert_fs::NamedTempFile::new("sample.txt")?;
+        file.write_str("A test\nActual content\nMore content\nAnother test")?;
+
+        let mut cmd = Command::cargo_bin("grrs")?;
+        cmd.arg("test").arg(file.path());
+        cmd.assert()
+            .success()
+            .stdout(predicate::str::contains("A test\nAnother test"));
+        Ok(())
+    }
+
+    // #[test]
+    // fn empty_string_as_pattern() -> Result<(), dyn Error> {
+    //     let file = NamedTempFile::new("sample.text")?;
+    //     file.write_str("A test\nActual content\nMore content\nAnother test")?;
+    //
+    //     let mut cmd = Command::cargo_bin("grrs")?;
+    //     cmd.arg("test").arg(file.path());
+    //     cmd.assert()
+    //         .success()
+    //         .stdout(predicate::str::contains(""));
+    //     Ok(())
+    // }
 
 }
